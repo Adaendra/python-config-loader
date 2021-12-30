a5733771b7f3f6e635cd336adbe91679c694aec8
Maxime Guilbert <mx.guilbert@gmail.com>
2021-12-29 22:27:49 -0500

    WIP - Config Loader [3] - Config loader

--- src/adaendra-python-config-loader/AdaendraConfigsLoader.py
+++ src/adaendra-python-config-loader/AdaendraConfigsLoader.py
@@ -8,6 +8,7 @@ from AdaendraConfigs import AdaendraConfigs
 # - [ ] let some env variable available to update project files to load and their type
 # - [ ] Faire en sorte qu'on puisse charger les fichiers comme dans Spring (application.yaml par défaut, application-env.yaml si il y a un environnement de défini...)
 
+
 def get_complete_config_file_path(file_name: str) -> str:
     config_folder = "" # TODO : Define + constante
     # TODO : Ajouter une condition pour définir le chemin où se trouvent les fichiers de base
@@ -17,11 +18,13 @@ def get_complete_config_file_path(file_name: str) -> str:
     # TODO : Ajouter une condition pour pouvoir charger du JSON à la place.
     return config_folder + file_name + ".yaml" # TODO : Contantes
 
+
 def get_file_list() -> list:
     file_list = []
 
     file_list.append(get_complete_config_file_path("application")) # TODO : Contante
-    # TODO : ajouter conditions pour extension + noms de projets
+    # TODO : ajouter conditions pour environnements + noms de projets
+
 
 def load_configs():
     config_dict = {}
