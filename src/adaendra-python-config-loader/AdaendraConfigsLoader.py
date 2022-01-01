c69494a118e62d9e7cfbf715f15aa3b64ff0273f
Maxime Guilbert <mx.guilbert@gmail.com>
2022-01-01 13:20:39 -0500

    WIP - Config Loader [6] - Config loader

--- src/adaendra-python-config-loader/AdaendraConfigsLoader.py
+++ src/adaendra-python-config-loader/AdaendraConfigsLoader.py
@@ -6,30 +6,42 @@ from AdaendraConfigs import AdaendraConfigs
 # TODO : Documentation (readme et tout)
 # TODO : Commentaires + ménage code
 
-config_folder = "./app/resources"  # TODO : Constantes
-if os.getenv('CONFIG_FOLDER') is not None:
-    config_folder = os.getenv('CONFIG_FOLDER')
 
-config_project_name = "application" # TODO : Constantes
-if os.getenv('CONFIG_PROJECT_NAME') is not None:
-    config_project_name = os.getenv('CONFIG_PROJECT_NAME')
+def get_config_folder() -> str:
+    config_folder = "./app/resources"  # TODO : Constantes
+    if os.getenv('CONFIG_FOLDER') is not None:
+        config_folder = os.getenv('CONFIG_FOLDER')
+    return config_folder
 
-config_file_extension = ".yaml" # TODO : Constantes
-if os.getenv('CONFIG_FILE_EXTENSION') is not None:
-    config_file_extension = os.getenv('CONFIG_FILE_EXTENSION')
 
-config_environment = None # TODO : Constantes
-if os.getenv('CONFIG_ENVIRONMENT') is not None:
-    config_environment = os.getenv('CONFIG_ENVIRONMENT')
+def get_config_project_name() -> str:
+    config_project_name = "application" # TODO : Constantes
+    if os.getenv('CONFIG_PROJECT_NAME') is not None:
+        config_project_name = os.getenv('CONFIG_PROJECT_NAME')
+    return config_project_name
+
+
+def get_config_file_extension() -> str:
+    config_file_extension = ".yaml" # TODO : Constantes
+    if os.getenv('CONFIG_FILE_EXTENSION') is not None:
+        config_file_extension = os.getenv('CONFIG_FILE_EXTENSION')
+    return config_file_extension
+
+
+def get_config_environment() -> str:
+    config_environment = None # TODO : Constantes
+    if os.getenv('CONFIG_ENVIRONMENT') is not None:
+        config_environment = os.getenv('CONFIG_ENVIRONMENT')
+    return config_environment
 
 
 def get_file_list() -> list:
     # The common config file
-    file_list = [config_folder + "/" + config_project_name + config_file_extension]
+    file_list = [get_config_folder() + "/" + get_config_project_name() + get_config_file_extension()]
 
     # The environment config file
-    if config_environment is not None:
-        file_list.append(config_folder + "/" + config_project_name + "-" + config_environment + config_file_extension)
+    if get_config_environment() is not None:
+        file_list.append(get_config_folder() + "/" + get_config_project_name() + "-" + get_config_environment() + get_config_file_extension())
 
     return file_list
 
